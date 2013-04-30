642704bc83c230a8029e347c59657e93821fbe64
Ben Forrest <ben@silvabox.com>
2013-04-30 19:06:45 +0100

    refactored rspecs a little

--- lib/gamification.rb
+++ lib/gamification.rb
@@ -2,9 +2,8 @@
 require 'active_support/all'
 
 class Gamification
-  def initialize(occurrences, attendances)
-    @occurrences = occurrences
-    @attendances = attendances
+  def initialize(context)
+    @context = context
     @points = 0
   end
 
@@ -16,7 +15,7 @@ class Gamification
   end
 
   def one_attendance(attendance)
-    @occurrences.each do |occurrence|
+    @context[:occurrences].each do |occurrence|
       @points += 1 if occurrence.start == attendance.occurrence.start
     end
   end
@@ -25,7 +24,7 @@ class Gamification
     month_start =  attendance.occurrence.start.beginning_of_month
     month_end = attendance.occurrence.start.end_of_month
 
-    attendances_in_month = @attendances.reject do |a|
+    attendances_in_month = @context[:attendances].reject do |a|
       a.occurrence.start
       a.occurrence.start < month_start || a.occurrence.start > month_end || a.occurrence.start == attendance.occurrence.start
     end
@@ -37,14 +36,14 @@ class Gamification
     month_end = attendance.occurrence.start.end_of_month
 
 # find all occurrences in the month
-    occurrences_in_month = @occurrences.reject do |o|
+    occurrences_in_month = @context[:occurrences].reject do |o|
       o.start < month_start || o.start > month_end
     end
     occurrences_in_month.reject! do |o|
       @att
     end
     @points += 2 if occurrences_in_month.all? do |o|
-      @attendances.any? { |a| a.occurrence.start == o.start }
+      @context[:attendances].any? { |a| a.occurrence.start == o.start }
     end
 
   end
--- spec/gamification_spec.rb
+++ spec/gamification_spec.rb
@@ -4,54 +4,44 @@ require "ice_cube"
 describe 'Gamification' do
   let(:time) { Time.now.beginning_of_month }
 
-  it 'should award 1 point for an attendance' do
-    attendances = create_attendances(time)
-
-    gamification = Gamification.new(create_occurrences, attendances)
-
-    attendance = attendances.first
-    
-    gamification.run(attendance).should eq 1
+  context 'given 1 attendance' do
+    it 'awards 1 point' do  
+      create_gamification create_attendances(time)
+      @gamification.run(@attendance).should eq 1
+    end
   end
 
-  it 'should award 1 extra point for one extra attendance in same month' do
-    attendances = create_attendances(time, time.advance(days: 7))
-
-    gamification = Gamification.new(create_occurrences, attendances)
-
-    attendance = attendances.first
-    
-    gamification.run(attendance).should eq 2
+  context 'given 2 attendances in same month' do
+    it 'awards 1 extra point' do
+      create_gamification create_attendances(time, time.advance(days: 7))
+      @gamification.run(@attendance).should eq 2
+    end
   end
 
-  it 'should award 2 extra points for two extra attendances in same month' do
-    attendances = create_attendances(time, time.advance(days: 7), time.advance(days: 14))
-
-    gamification = Gamification.new(create_occurrences, attendances)
-
-    attendance = attendances.first
-    
-    gamification.run(attendance).should eq 3
+  context 'given 3 attendances in same month' do
+    it 'awards 2 extra points' do
+      create_gamification create_attendances(time, time.advance(days: 7), time.advance(days: 14))
+      @gamification.run(@attendance).should eq 3
+    end
   end
 
-  it 'should award 3 extra points for three extra attendances in same month' do
-    attendances = create_attendances(time, time.advance(days: 7), time.advance(days: 14), time.advance(days: 21))
-
-    gamification = Gamification.new(create_occurrences, attendances)
-
-    attendance = attendances.first
-    
-    gamification.run(attendance).should eq 4
+  context 'given 4 extra attendances in same month' do  
+    it 'awards 3 extra points' do
+      create_gamification create_attendances(time, time.advance(days: 7), time.advance(days: 14), time.advance(days: 21))
+      @gamification.run(@attendance).should eq 4
+    end
   end
 
-  it 'should award 2 extra points for complete attendances in month' do
-    attendances = create_attendances(time, time.advance(days: 7), time.advance(days: 14), time.advance(days: 21), time.advance(days: 28))
+  context 'given complete attendance in month' do
+    it 'awards 2 extra points' do
+      create_gamification create_attendances(time, time.advance(days: 7), time.advance(days: 14), time.advance(days: 21), time.advance(days: 28))
+      @gamification.run(@attendance).should eq 7
+    end
+  end
 
-    gamification = Gamification.new(create_occurrences, attendances)
-
-    attendance = attendances.first
-    
-    gamification.run(attendance).should eq 7
+  def create_gamification(attendances)
+    @gamification = Gamification.new(Context.new(occurrences: create_occurrences, attendances: attendances))
+    @attendance = attendances.first
   end
 
   def create_occurrences
@@ -78,10 +68,5 @@ describe 'Gamification' do
   def create_attendance(start, name)
     attendance = double :attendance
     attendance.should_receive(:occurrence).at_least(1).times.and_return create_occurrence(time, :attendance)
-  end
-
-  def create_double(start, name, type)
-    occurrence = double "#{type}_#{name}_#{start.strftime('%Y%m%d%H%M')}".to_sym
-    occurrence.should_receive(:start).at_least(1).times.and_return(start)
   end
 end
